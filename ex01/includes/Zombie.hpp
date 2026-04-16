28a3b623e2817bb9fa822a6d4c92666fd363e3d2
Woojeong <alsxmakt123@naver.com>
2026-04-16 15:21:12 +0200

    pret

--- ex01/includes/Zombie.hpp
+++ ex01/includes/Zombie.hpp
@@ -16,4 +16,6 @@ class Zombie
     void  setName(std::string n);
 };
 
+Zombie* zombieHorde(int N, std::string name);
+
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,12 +7,12 @@ int main(void)
   std::string*  stringPTR = &str;
   std::string&  stringREF = str;
 
+  std::cout << "address:"<< std::endl;
   std::cout << "The memory address of str: " << &str << std::endl;
   std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
   std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
 
-  std::cout << "--------------------------------------------------------------------"<< std::endl;
-
+  std::cout << "value:"<< std::endl;
   std::cout << "The value of str: " << str << std::endl;
   std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
   std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
